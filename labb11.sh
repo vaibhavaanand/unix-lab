105e0ddde7ecf3533443a9bb0b07f15ada8fa53e
vaibhavaanand <35593993+vaibhavaanand@users.noreply.github.com>
2018-04-03 14:56:41 +0530

    Add files via upload

---
+++ labb11.sh
@@ -0,0 +1,6 @@
+$ dialog --title "hello" --msgbox "Hi" 10 20
+$ dialog --title "Hello" --yesno "happy" 10 20
+$ dialog --title "Hello" --infobox "happy" 10 20;sleep 6
+$ dialog --title "Hello" --menu "Enter the option" 10 20 3 1 red, 2 green, 3 blue
+$ dialog --title "Hello" --checklist "tick the list" 10 20 3 1 red on, 2 green off, 3 blue on
+
